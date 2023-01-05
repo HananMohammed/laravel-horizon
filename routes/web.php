eca9d51c0274fcfa9ae5294f7662081fb402f7a5
hananamin111995 <102970138+hananamin111995@users.noreply.github.com>
2023-01-05 14:50:14 +0200

    setup predis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Redis::set('name', 'Hanan');
+    $name = Redis::get('name');
+    dd($name);
     return view('welcome');
 });
